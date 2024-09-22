95c9c99c3e040ea092de9552fb968a847e326030
Zalez95 <12982523+Zalez95@users.noreply.github.com>
2024-09-22 18:20:09 +0200

    Update conan and cmake files to conan2

--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,33 @@
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.files import copy
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
 
 class StdExtConan(ConanFile):
 	name = "stdext"
-	version = "v1.0"
+	version = "v1.0.0"
 	license = "MIT"
 	author = "Zalez95"
 	settings = "os", "compiler", "build_type", "arch"
 	options = {"shared": [True, False], "fPIC": [True, False]}
 	default_options = {"shared": False, "fPIC": True}
-	generators = "cmake"
 
 	def config_options(self):
 		if self.settings.os == "Windows":
 			del self.options.fPIC
 
+	def layout(self):
+		cmake_layout(self)
+
 	def export_sources(self):
-		self.copy("*")
+		copy(self, "LICENSE", self.recipe_folder, self.export_sources_folder)
+		copy(self, "*.txt", self.recipe_folder, self.export_sources_folder)
+		copy(self, "*.cpp", self.recipe_folder, self.export_sources_folder)
+		copy(self, "*.h", self.recipe_folder, self.export_sources_folder)
+		copy(self, "*.hpp", self.recipe_folder, self.export_sources_folder)
+
+	def generate(self):
+		tc = CMakeToolchain(self)
+		tc.generate()
 
 	def build(self):
 		cmake = CMake(self)
@@ -27,6 +39,4 @@ class StdExtConan(ConanFile):
 		cmake.install()
 
 	def package_info(self):
-		self.cpp_info.includedirs = ["include"]
-		self.cpp_info.libdirs = ["lib"]
-		self.cpp_info.libs = [self.name]
+		self.cpp_info.libs = ["stdext"]
